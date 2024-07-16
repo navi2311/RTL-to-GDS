47530abf7ff3ede000cfce9bf880d4f43c02660c
Naveen <134842758+navi2311@users.noreply.github.com>
2024-07-15 21:26:22 -0700

    Update my_alu.v

--- 2)RTL/mini_project_1/my_alu.v
+++ 2)RTL/mini_project_1/my_alu.v
@@ -1,13 +1,15 @@
 `default_nettype none
 `include "full_adder.v"
 module my_alu #(
-	parameter int WIDTH=4,
-    parameter int OFF=0,
-  	parameter int ADD=1,
-    parameter int SUB=2,
-    parameter int NO_OP=3
+	parameter  WIDTH=4,
+    parameter  OFF=0,
+  	parameter  ADD=1,
+    parameter  SUB=2,
+    parameter  NO_OP=3
 )
   (
+    input wire clk,
+    input wire rst,
     input wire [(WIDTH-1):0]A,
     input wire [(WIDTH-1):0]B,
     input wire [1:0]alu_op,
@@ -15,42 +17,56 @@ module my_alu #(
     output reg flag);
   wire [3:0]sum_w;
   wire carry_w;
-  full_adder dut(.A(A),
+
+  full_adder dut(.clk(clk),
+                 .rst(rst),
+                 .A(A),
                  .B(B),
                  .sum(sum_w),
                  .carry(carry_w));
-  
-  always@(*) begin
-    case(alu_op)
-      OFF:begin
-        out=4'b0;
-        flag=1'b0;
-      end
-      ADD:begin
-        out<=sum_w;
-        flag<=carry_w;
-      end
-      SUB:begin
-        if(A>=B) begin
-          out<=A-B;
-          flag=1'b0;
-        end
-        else begin
-          out<=B-A;
-          flag=1'b1;
-        end
+ 
+  always@(posedge clk) begin
+    if(rst) begin
+      out<=4'b0;
+      flag<=1'b0;
+    end
+    else begin
+    	case(alu_op)
+      		OFF:begin
+        		out<=4'b0;
+        		flag<=1'b0;
+      		end
+      		ADD:begin
+              	//a_temp=A;
+                //b_temp<=B;
+        		out<=sum_w;
+        		flag<=carry_w;
+      		end
+      		SUB:begin
+        		if(A>=B) begin
+          			out<=A-B;
+          			flag<=1'b0;
+        		end
+        		else begin
+          			out<=B-A;
+          			flag<=1'b1;
+        		end
         
-      end
-      NO_OP:begin
-        //assuming it keeps perviosu values only
-        out<=out;
-        flag<=flag;
-      end
-      default: begin
-        out<=4'bz;
-        flag<=4'bz;
-      end
-    endcase
+      		end
+      		NO_OP:begin
+        	//assuming it keeps perviosu values only
+        		out<=out;
+        		flag<=flag;
+      		end
+      		default: begin
+        		out<=4'bz;
+        		flag<=4'bz;
+      		end
+    	endcase
+    end
     
   end
+  
+  
+  
 endmodule
