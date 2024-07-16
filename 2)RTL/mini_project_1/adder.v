812dddf8fd017e7245e41ce6f519830ab6f3a620
Naveen <134842758+navi2311@users.noreply.github.com>
2024-07-15 21:27:05 -0700

    Update adder.v

--- 2)RTL/mini_project_1/adder.v
+++ 2)RTL/mini_project_1/adder.v
@@ -1,13 +1,24 @@
 // Code your design here
 `default_nettype none
-module adder(input wire A,
+module adder(
+  			input clk,
+  			input rst,
+  			input wire A,
             input wire B,
             input wire cin,
-            output wire sum,
-            output wire carry);
+            output reg sum,
+            output reg carry);
   
-  
-  assign sum=A ^ B ^ cin;
-  assign carry=(A & B) | (B & cin) | (cin & A);
+  always@(posedge clk) begin
+    if(rst) begin
+      sum <=1'b0;
+      carry <=1'b0;
+    end
+    else begin
+      sum=A ^ B ^ cin;
+      carry=(A & B) | (B & cin) | (cin & A);
+    end
+  end
+   
   
 endmodule
